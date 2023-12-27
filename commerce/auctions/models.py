10baa11aa2162865cc1ca1dd8f1bc64171948a09
MeanMachineRex 章魚貓 <RexRowan@users.noreply.github.com>
2023-12-27 16:09:39 +0000

    models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,32 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} ({'Active' if self.active else 'Closed'})"
+
+class Bid(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} bid on {self.listing.title} with ${self.bid_amount}"
+
+class Comment(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    comment_text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} commented on {self.listing.title}"
